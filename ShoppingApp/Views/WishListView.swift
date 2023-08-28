a1df7ad1dee2472d6d7b8ab3cd69a92e0fde3b14
soyeon <bmesjl5700@gmail.com>
2023-08-28 16:51:50 -0700

    Redesign the Wishlist View to be similar to CartView.

--- ShoppingApp/Views/WishListView.swift
+++ ShoppingApp/Views/WishListView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct WishListView: View {
     
     @EnvironmentObject var cartManager: ProductManager
+    @Environment(\.presentationMode) var presentationMode
+
     
     
     var columns = [
@@ -21,30 +23,63 @@ struct WishListView: View {
     var body: some View {
         
         VStack {
-            HStack {
-                
-                Text("Wish List")
+            VStack(spacing:1) {
+                Text("AU")
                     .bold()
-                    .font(.system(size: 20))
-                
-                
-            } .padding(.top, 20)
+                    .font(.system(size:16))
+                    
+                        
+                Text("WishList")
+                    .bold()
+                    .font(.system(size:20))
+
+            }
+            
+            Divider()
             
             ScrollView {
                 
-                LazyVGrid(columns: columns, spacing: 30) {
+                LazyVGrid(columns: columns, spacing: 50) {
                     
                     if cartManager.savedProducts.count > 0 {
                         let uniquesavedProducts = Array(Set(cartManager.savedProducts))
                         
-                        LazyVGrid(columns: columns) {
+                        LazyVGrid(columns: columns, spacing: 10) {
                             ForEach(uniquesavedProducts, id: \.id) {
-                                product in
+                                savedProduct in
                                 
-                                SaveProductRowView(product: product)
+                                SaveProductRowView(product: savedProduct, userSelection: UserSelectionModel(productInfo: products[0], productSize: "XS", productColor: "White"))
+                                    .padding(.top,20)
                                 
                             }
-                        } }
+                            
+                        } } else {
+                            VStack {
+                                Text("Your WishList is currently empty.")
+                                    .bold()
+                                    .padding(.top,30)
+                                
+                                Button(action: {
+                                    
+                                    presentationMode.wrappedValue.dismiss() //dismiss the screen
+                                    
+                                    
+                                }, label: {
+                                    Text("Shop Now")
+                                        .foregroundColor(.black)
+                                    
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 16)
+                                                .stroke(.black, lineWidth: 1)
+                                                .frame(width: 150, height: 30)
+                                        )
+                                        .padding()
+                                })
+                                
+                            }
+                            .frame(width: 300)
+
+                        }
                 }
                 .padding(.leading, 200)
                 
