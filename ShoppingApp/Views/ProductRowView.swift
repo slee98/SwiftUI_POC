65ab0d60b4053a814e0e0643e5d3c95e008cad54
soyeon <bmesjl5700@gmail.com>
2023-08-28 17:04:11 -0700

    Implemented retrieval of user selections from the 'UserSelectionModel', allowing access to saved information such as selected sizes and colors.

--- ShoppingApp/Views/ProductRowView.swift
+++ ShoppingApp/Views/ProductRowView.swift
@@ -9,45 +9,73 @@ import SwiftUI
 
 struct ProductRowView: View {
     
-    @EnvironmentObject var cartmanager: ProductManager    
+    @EnvironmentObject var cartmanager: ProductManager
+
     var product: Product
-    
+    var userSelection: UserSelectionModel
+
     
     var body: some View {
-        HStack() {
-            Image(product.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(10)
-                .frame(width: 70)
-                .padding(.leading, 20)
+        HStack(spacing: 5) {
+            
+                Image(product.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(5)
+                    .frame(width: 90)
+                   
+                
             
             
-            VStack (alignment: .leading) {
-                Text(product.name)
-                    .bold()
+            VStack (alignment: .leading, spacing: 3) {
+                HStack{
+                    Text(product.name)
+                        .font(.system(size: 14))
+                    Spacer()
+                    Text("$\(product.price * (cartmanager.arrangedProducts[userSelection] ?? 0))")
+                        .font(.system(size: 14))
+                      
+                }
+                
                 
                 Text("$\(product.price).00")
-                    .font(.body)
+                    .font(.system(size: 14))
+                    .bold()
                 
-                
-                Text("Quantity: \(cartmanager.arrangedProducts[product] ?? 0)")
+                HStack {
+                    Text("Color: \(userSelection.productColor)")
+                        .foregroundColor(.black)
+                        .font(.caption)
+                    
+                    Spacer()
+                    Button ( action: {
+                        
+                        cartmanager.removeFromCart(product)
+                        
+                        
+                    }){
+                        Image(systemName: "trash")
+                            .foregroundColor(Color(hue: 1.0, saturation: 0.89, brightness: 0.835))
+                           
+                    }
+                }
+
+                Text("Size: \(userSelection.productSize)")
                     .foregroundColor(.black)
                     .font(.caption)
                 
-            }  .frame(width: 240)
-            
-            Button ( action: {
+           
+               
+                Text("QTY: \(cartmanager.arrangedProducts[userSelection] ?? 0)")
+                    .foregroundColor(.black)
+                    .font(.caption)
                 
-                cartmanager.removeFromCart(product)
-                
-                
-            }){
-                Image(systemName: "trash")
-                    .foregroundColor(Color(hue: 1.0, saturation: 0.89, brightness: 0.835))
             }
+            .padding(.bottom, 20)
+            .frame(width: 250)
+            
+            
         }
-        .padding(.trailing, 30)
     }
     
     
@@ -55,7 +83,8 @@ struct ProductRowView: View {
 
 struct ProductRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductRowView(product:products[0])
+        ProductRowView(product:products[0], userSelection: UserSelectionModel(productInfo: products[0], productSize: "XS", productColor: "Brown"))
             .environmentObject(ProductManager())
+
     }
 }
