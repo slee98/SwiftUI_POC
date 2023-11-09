0e39a80679e97f4a8d5775b58819492cf2ec5d98
soyeon <bmesjl5700@gmail.com>
2023-11-09 14:03:12 -0800

    Upload File

---
+++ ShoppingApp/Models/UserSelectionModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserSelectionModel.swift
+//  ShoppingApp
+//
+//  Created by Soyeon Lee on 11/9/23.
+//
+
+import Foundation
---
+++ ShoppingApp/Views/CategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  Categories.swift
+//  ShoppingApp
+//
+//  Created by Soyeon Lee on 11/9/23.
+//
+
+import SwiftUI
+
+struct Categories: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Categories()
+}
